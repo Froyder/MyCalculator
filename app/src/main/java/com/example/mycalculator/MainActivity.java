a0ffabafdf8b1605b8ec2574c1038e648080106f
Froyder <homelik@mail.ru>
2021-04-13 19:47:04 +0300

    added strings

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.mycalculator;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
